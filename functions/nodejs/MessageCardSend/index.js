7d72a350729c32d989efdc866450196337c6f07a
Ethan Zhao <hi@yizhe.me>
2024-05-11 16:15:50 +0800

    更新远端分支与 git 保持一致

--- functions/nodejs/MessageCardSend/index.js
+++ functions/nodejs/MessageCardSend/index.js
@@ -8,45 +8,57 @@ const { newLarkClient } = require('../utils');
  * @return 函数的返回数据
  */
 module.exports = async function (params, context, logger) {
-    const { receiver_type, receiver_id, message_type, message_content } = params;
-    const receiveIdTypes = new Set(['open_id', 'user_id', 'email', 'chat_id']);
+  // 成功案例
+  // const client = await newLarkClient({ userId: context.user._id }, logger);
+  // try {
+  //   const response = await client.im.message.create({
+  //     params: {
+  //       receive_id_type: "chat_id",
+  //     },
+  //     data: {
+  //       receive_id: "oc_de4d7e6d3ef4d77a1021a8cbd875d1b1",
+  //       msg_type: "share_chat",
+  //       content: "{\"chat_id\":\"oc_156ec5f8227af8a67d7b2e5fc97ce704\"}",
+  //     }
+  //   });
+  //   console.log({ response })
+  //   return response;
+  // } catch (e) {
+  //   throw new Error(e)
+  // }
+  const { receive_id_type, receive_id, msg_type, content } = params;
+  const receiveIdTypes = new Set(['open_id', 'user_id', 'email', "union_id", 'chat_id']);
 
-    // 判断 receiver_type 是否合法
-    if (!receiveIdTypes.has(receiver_type)) {
-        logger.error(`错误的 receiver_type 类型: ${receiver_type}`);
-        return { code: -1, receiver_id, message: '错误的 receiver_type 类型' };
+  // 判断 receive_id_type 是否合法
+  if (!receiveIdTypes.has(receive_id_type)) {
+    logger.error(`错误的 receive_id_type 类型: ${receive_id_type}`);
+    throw new Error('错误的 receive_id_type 类型')
+  }
+
+  // 判断 receive_id 和 content 是否为空
+  if (!receive_id || !msg_type || !content) {
+    logger.error(
+      `receive_id 或 content 不能为空. Received - receive_id: ${receive_id}, msg_type: ${msg_type}, content: ${content}`,
+    );
+    throw new Error('receive_id 或 content 或 msg_type 不能为空')
+  }
+
+  const client = await newLarkClient({ userId: context.user._id }, logger);
+  try {
+    let response = await client.im.message.create({
+      params: { receive_id_type },
+      data: {
+        receive_id,
+        msg_type,
+        content
+      },
+    });
+
+    if (response.code !== 0) {
+      throw new Error("消息发送失败")
     }
-
-    // 判断 receiver_id 和 message_content 是否为空
-    if (!receiver_id || !message_type || !message_content) {
-        logger.error(
-            `receiver_id 或 message_content 不能为空. Received - receiver_id: ${receiver_id}, message_type: ${message_type}, message_content: ${message_content}`,
-        );
-        return { code: -1, receiver_id, message: 'receiver_id 或 message_content 不能为空' };
-    }
-
-    const client = await newLarkClient({ userId: context.user._id }, logger);
-
-    try {
-        let response = await client.im.message.create({
-            params: {
-                receiveIdTypes: receiver_type,
-            },
-            data: {
-                receive_id: receiver_id,
-                msg_type: message_type, // text interactive
-                content: message_content,
-            },
-        });
-
-        if (response.code !== 0) {
-            logger.error(`发送消息失败: ${response.msg}`);
-            return { code: -1, receiver_id, message: `发送消息失败: ${response.msg}`, details: response.data };
-        }
-
-        return { code: 0, message: '发送消息成功', data: response.data };
-    } catch (e) {
-        logger.error('发送消息失败', { error: e });
-        return { code: -1, receiver_id, message: '发送消息失败', e };
-    }
+    return { code: 0, message: '发送消息成功', data: response.data };
+  } catch (e) {
+    throw new Error(e)
+  }
 };
--- functions/nodejs/MessageContentGenerator/index.js
+++ functions/nodejs/MessageContentGenerator/index.js
@@ -1,7 +1,7 @@
 // 通过 NPM dependencies 成功安装 NPM 包后此处可引入使用
 // 如安装 linq 包后就可以引入并使用这个包
 // const linq = require("linq");
-
+const { newLarkClient } = require('../utils');
 /**
  * @param {Params}  params     自定义参数
  * @param {Context} context    上下文参数，可通过此参数下钻获取上下文变量信息等
@@ -10,45 +10,136 @@
  * @return 函数的返回数据
  */
 module.exports = async function (params, context, logger) {
-    // 日志功能
-    logger.info(`${new Date()} 消息卡片内容生成器函数开始执行...`);
+  // 日志功能
+  logger.info(`${new Date()} 消息卡片内容生成器函数开始执行...`);
+  // https://open.feishu.cn/document/server-docs/im-v1/message-content-description/create_json#45e0953e
+  // https://open.feishu.cn/document/server-docs/im-v1/message/create?appId=cli_a68809f3b7f9500d
 
-    const { message_def } = params;
-    logger.info(params);
+  const { message_def, group } = params;
+  logger.info(params);
+  const groupIds = group.map(item => item._id);
+  const chats = await application.data.object("object_feishu_chat")
+    .where({
+      _id: application.operator.in(groupIds.join(","))
+    }).select("chat_id").find();
+  const chatIds = chats.map(i => i.chat_id);
+  logger.info({ groupIds, chatIds, ids: groupIds.join(',') });
 
-    const fields = await application.metadata.object('object_chat_message_def').getFields();
-    const fieldApiNames = fields.map(item => item.apiName);
-    const record = await application.data.object('object_chat_message_def').select(fieldApiNames).where({ _id: message_def._id }).findOne();
+  const client = await newLarkClient({ userId: context.user._id }, logger);
 
-    logger.info(`record: ${JSON.stringify(record, null, 2)}`);
+  const getImageKey = async (images) => {
+    const res = await client.im.image.create({
+      data: {
+        image_type: "message",
+        image: images[0]
+      }
+    })
+    logger.info(res);
+  };
 
-
-
-    const messageType = record.option_message_type;
-
+  const getContent = async (type) => {
     // 对消息类型进行判断
-    switch (messageType) {
-        case 'option_rich_text':
-            // 富文本类型消息
-            // todo: 完成富文本到消息卡片类型的转换
+    switch (type) {
+      case 'option_rich_text':
+        // 富文本类型消息
+        const postData = transformRichText(message_def.message_richtext.raw);
+        return {
+          msg_type: "post",
+          content: JSON.stringify(postData)
+        };
+      case 'option_video':
+        // 视频类型消息直接发成文本类型
+        const textObj = { text: `${message_def.video_content} ${message_def.video_url}` }
+        return {
+          msg_type: "text",
+          content: JSON.stringify(textObj)
+        };
+      case 'option_card':
+        // 消息卡片模板类型消息
+        const data = {
+          type: 'template',
+          data: {
+            template_id: message_def.message_template_id,
+          }
+        };
+        return {
+          msg_type: "interactive",
+          content: JSON.stringify(data)
+        };
+      default:
+        // 图片类型
+        await getImageKey(message_def.images);
+        return {
+          msg_type: "image",
+          content: JSON.stringify({ image_key: message_def.images[0].token })
+        };
+    };
+  }
 
-            break;
-        case 'option_video':
-            // 视频类型消息
-            card = {
-                "text": record.video_content + record.video_url
-            }
+  const res = await getContent(message_def.option_message_type);
+  const paramsData = {
+    ...res,
+    receive_id: chatIds[0],
+    receive_id_type: "chat_id"
+  };
+  logger.info(JSON.stringify(paramsData, 0, 2));
 
-            return JSON.stringify(card);
-        case 'option_card':
-            // 消息卡片模板类型消息
-            card = {
-                type: 'template',
-                data: {
-                    template_id: record.message_template_id,
-                },
-            };
+  try {
+    await faas.function('MessageCardSend').invoke({ ...paramsData });
+  } catch (error) {
+    throw new Error(error)
+  }
+};
 
-            return JSON.stringify(card);
+// 转换富文本
+const transformRichText = (html) => {
+  const content = [];
+
+  const getTag = (content) => {
+    if (/<a/.test(content)) {
+      return "a";
     }
+    if (/<img/.test(content)) {
+      return "img"
+    }
+    return "text"
+  };
+
+  // 匹配所有<div>标签并提取其内容和样式
+  const divRegex = /<div[^>]*>(.*?)<\/div>/g;
+  let match;
+  while ((match = divRegex.exec(html)) !== null) {
+    const divContent = match[1].trim();
+    if (divContent !== "") {
+      const element = [];
+      let style = [];
+
+      // 检查是否包含样式
+      if (/<b>/.test(divContent)) style.push("bold");
+      if (/<i>/.test(divContent)) style.push("italic");
+      if (/<u>/.test(divContent)) style.push("underline");
+      if (/<s>/.test(divContent)) style.push("lineThrough");
+      // if (/<img/.test(divContent)) style.push("image");
+
+      // 提取文本内容
+      const text = divContent.replace(/<\/?[^>]+(>|$)/g, "");
+      if (text !== "") {
+        let data = { tag: getTag(divContent), text };
+        if (style.length > 0) {
+          data = { ...data, style };
+        }
+        if (/<a/.test(divContent)) {
+          const matchHref = divContent.match(/href=\"(.*?)\"/);
+          data = { ...data, href: matchHref[1] };
+        }
+        element.push(data);
+      }
+
+      if (element.length > 0) {
+        content.push(element);
+      }
+    }
+  }
+  console.log({ content });
+  return { zh_cn: { content } };
 };
