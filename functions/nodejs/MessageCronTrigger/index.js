fa4100f6b4c0394e3496a029c0c17de71c488049
wangshujian <770257701@qq.com>
2024-05-15 16:37:31 +0800

    任务一键催办&任务创建重试

--- functions/nodejs/MessageCronTrigger/index.js
+++ functions/nodejs/MessageCronTrigger/index.js
@@ -1,5 +1,4 @@
 // 通过 NPM dependencies 成功安装 NPM 包后此处可引入使用
-// 如安装 linq 包后就可以引入并使用这个包
 const dayjs = require('dayjs');
 const _ = application.operator;
 
@@ -14,8 +13,8 @@ module.exports = async function (params, context, logger) {
     // 日志功能
     logger.info(`${new Date()} 消息触发器函数开始执行`);
 
-    const currentTime = dayjs().add(8, 'hour').valueOf(); // 当前时间
-    const timeBuffer = 1000 * 60 * 5; // 5 minutes buffer
+    const currentTime = dayjs().valueOf(); // 当前时间，东八区时间
+    const timeBuffer = 1000 * 60 * 60; // 5 minutes buffer
     logger.info('当前时间->', currentTime, dayjs(currentTime).format('YYYY-MM-DD HH:mm:ss'));
 
     // 查询所有的消息定义数据
@@ -53,43 +52,77 @@ module.exports = async function (params, context, logger) {
             )
         )
         .find();
+
     logger.info('查询到的消息定义数量->', messageDefineRecords.length);
     logger.info(messageDefineRecords);
 
-
-    // 根据消息定义判断是否需要调用消息生成函数
-
-    // 一次性消息
+    // 一次性任务
     const valuedOnceMessageDefineList = messageDefineRecords.filter(item => item.option_method === 'option_once' && item.boolean_public_now === false);
     logger.info('需要触发的一次性消息定义数量->', valuedOnceMessageDefineList.length);
 
-    // 周期消息
-    const calculateTriggerTime = (startTime, repetitionRate, unit) => {
-        return dayjs(startTime).add(repetitionRate, unit).valueOf();
-    };
+    // 周期性任务
+    const valuedCycleMessageDefineList = [];
+    const cycleMessageDefineList = messageDefineRecords.filter(item => item.option_method === 'option_cycle');
 
-    const valuedCycleMessageDefineList = messageDefineRecords.filter(item => item.option_method === 'option_cycle').filter(cycleMessageDefine => {
-        const { datetime_start: startTime, option_time_cycle: cycleType, repetition_rate: repetitionRate } = cycleMessageDefine;
-        const unitMapping = {
-            'option_day': 'day',
-            'option_week': 'week',
-            'option_month': 'month',
-            'option_quarter': { unit: 'month', factor: 3 },
-            'option_half_year': { unit: 'month', factor: 6 },
-            'option_year': 'year'
-        };
+    cycleMessageDefineList.forEach(cycleMessageDefine => {
+        const { datetime_start: startTime, datetime_end: endTime, option_time_cycle: cycleType, repetition_rate: repetitionRate, title } = cycleMessageDefine;
+        const startDate = dayjs(startTime);
+        const endDate = dayjs(endTime);
+        console.info('当前循环的定义记录->', title, 'startDate', startDate.format('YYYY-MM-DD HH:mm:ss'), 'endDate', endDate.format('YYYY-MM-DD HH:mm:ss'), repetitionRate );
+        
+        let unit;
 
-        const { unit, factor = 1 } = typeof unitMapping[cycleType] === 'string' ? { unit: unitMapping[cycleType] } : unitMapping[cycleType];
-        const triggerTime = calculateTriggerTime(startTime, repetitionRate * factor, unit);
+        switch (cycleType) {
+            case 'option_day':
+                unit = 'day';
+                break;
+            case 'option_week':
+                unit = 'week';
+                break;
+            case 'option_month':
+                unit = 'month';
+                break;
+            case 'option_quarter':
+                unit = 'month';
+                repetitionRate *= 3;
+                break;
+            case 'option_half_year':
+                unit = 'month';
+                repetitionRate *= 6;
+                break;
+            case 'option_year':
+                unit = 'year';
+                break;
+            default:
+                return;
+        }
 
-        return triggerTime && currentTime >= triggerTime - timeBuffer && currentTime <= triggerTime + timeBuffer;
+        let currentDate = startDate;
+        while (currentDate.isBefore(endDate) || currentDate.isSame(endDate)) {
+            const triggerTime = currentDate.valueOf();
+            const hourMinute = startDate.format('HH:mm');
+            const dailyTriggerTime = dayjs(`${dayjs(currentTime).format('YYYY-MM-DD')} ${hourMinute}`).valueOf();
+            console.info('while循环内的触发时间', dayjs(dailyTriggerTime).format("YYYY-MM-DD HH:mm:ss"));
+
+            if (currentDate.format('YYYY-MM-DD') === dayjs(currentTime).format('YYYY-MM-DD')) {
+                if (currentTime >= dailyTriggerTime - timeBuffer && currentTime <= dailyTriggerTime + timeBuffer) {
+                    valuedCycleMessageDefineList.push(cycleMessageDefine);
+                }
+            } else {
+                if (currentTime >= triggerTime - timeBuffer && currentTime <= triggerTime + timeBuffer) {
+                    valuedCycleMessageDefineList.push(cycleMessageDefine);
+                }
+            }
+
+            currentDate = currentDate.add(repetitionRate, unit);
+        }
     });
 
     logger.info('需要触发的周期消息定义数量->', valuedCycleMessageDefineList.length);
 
     const valuedMessageDefineList = [...valuedOnceMessageDefineList, ...valuedCycleMessageDefineList];
     logger.info('✅ 需要触发的消息定义总数量->', valuedMessageDefineList.length);
-    return valuedMessageDefineList
+    return valuedMessageDefineList;
 
     // 创建一个函数，用于调用消息生成函数，最后使用 Promise.all 来并发执行 valuedMessageDefineList 内的消息定义
     const invokeMessageGenerateFunction = async messageDefine => {
